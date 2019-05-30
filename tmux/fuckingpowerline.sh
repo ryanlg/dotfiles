603752acda101b7fd5c08ac1cd103b674c376a44
Ryan Liang <xmomowan@gmail.com>
2019-05-29 23:03:29 -0500

    Fix Tmux's config, was broken in new version

--- tmux/fuckingpowerline.sh
+++ tmux/fuckingpowerline.sh
@@ -17,25 +17,22 @@ set -g status-interval 5
 # default statusbar colors
 set-option -g status-fg $textColor
 set-option -g status-bg $defaultStatus
-set-option -g status-attr default
+set-option -g status-style default
 
 # non-active window colors
-set-window-option -g window-status-fg $textColor
-set-window-option -g window-status-bg $secondaryBack
+setw -g window-status-style bg=$secondaryBack,fg=$textColor
 set -g window-status-format " #I #W "
 
 # active window title colors
-set-window-option -g window-status-current-fg $lightblue
-set-window-option -g window-status-current-bg $thirdBack
-set-window-option -g  window-status-current-format "    #I #[bold]#W    "
+setw -g window-status-current-style bg=$thirdBack,fg=$lightblue
+setw -g  window-status-current-format "    #I #[bold]#W    "
 
 # pane border colors
-set-window-option -g pane-border-fg $secondaryBack
-set-window-option -g pane-active-border-fg $separator
+setw -g pane-border-style fg=$secondaryBack
+setw -g pane-active-border-style fg=$separator
 
 # message text
-set-option -g message-bg $textColor
-set-option -g message-fg $secondaryBack
+set -g message-style bg=$textColor,fg=$secondaryBack
 
 # pane number display
 set-option -g display-panes-active-colour $secondaryBack
