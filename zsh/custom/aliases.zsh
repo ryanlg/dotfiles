052c4a3be975e4691976100e4a0519e5ca68c583
Ryan Liang <p@ryanl.io>
2021-01-07 21:10:36 -0600

    zshrc and aliases
    Signed-off-by: Ryan Liang <p@ryanl.io>

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -2,6 +2,7 @@ alias ws='cd ~/workspace'
 alias re='.. && -'
 alias c='clear'
 alias sha1='openssl sha1 '
+alias vim='nvim'
 
 # CS Department Remote Machine
 alias rlmount='sshfs rliang@best-linux.cs.wisc.edu:/u/r/l/rliang/private ~/workspace/mnt/rliang/private'
