08acaecaf5f6138369b4874a417196884b35083d
Ryan Liang <p@ryanl.io>
2021-10-20 15:04:04 -0400

    Add ssh!

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -9,3 +9,5 @@ alias rlmount='sshfs rliang@best-linux.cs.wisc.edu:/u/r/l/rliang/private ~/works
 alias rlumount='umount ~/workspace/mnt/rliang/private'
 alias rlprivate='cd ~/workspace/mnt/rliang/private'
 alias cpprivate='echo "/Users/ryan/workspace/mnt/rliang/private" | pbcopy'
+
+alias ssh!="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $ISILON_DUCT_TAPE_SSH_PRIVATE_KEY_LOCATION -l root"
