fe41a9e86ea9eb43fa48b12d40c2c9efe1f17172
Ryan Liang <p@ryanl.io>
2021-11-09 16:17:28 -0500

    Remove unused aliases, add ssh#

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,13 +1,10 @@
 alias ws='cd ~/workspace'
 alias re='.. && -'
 alias c='clear'
-alias sha1='openssl sha1 '
 alias vim='nvim'
 
-# CS Department Remote Machine
-alias rlmount='sshfs rliang@best-linux.cs.wisc.edu:/u/r/l/rliang/private ~/workspace/mnt/rliang/private'
-alias rlumount='umount ~/workspace/mnt/rliang/private'
-alias rlprivate='cd ~/workspace/mnt/rliang/private'
-alias cpprivate='echo "/Users/ryan/workspace/mnt/rliang/private" | pbcopy'
+# Log in with root and not saving to known host.
+alias ssh!="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root"
 
-alias ssh!="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $ISILON_DUCT_TAPE_SSH_PRIVATE_KEY_LOCATION -l root"
+# Log in without saving to known host, but you can customize the user.
+alias 'ssh#'="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
