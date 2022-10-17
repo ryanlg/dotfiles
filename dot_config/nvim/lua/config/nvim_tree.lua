16da98c5f1676c0e7a7895283bdf41a5fed77fa2
Ryan Liang <ryan_liang@dell.com>
2022-10-17 10:39:53 -0400

    Hide `__pycache__` and `.mypy_caches` in `nvim-tree`

--- dot_config/nvim/lua/config/nvim_tree.lua
+++ dot_config/nvim/lua/config/nvim_tree.lua
@@ -1,3 +1,13 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    filters = {
+        exclude = {
+            "\\.tox$",
+        },
+        custom = {
+            "^__pycache__$",
+            "^\\.mypy_cache$",
+        },
+    },
+})
 
 vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTreeToggle<CR>")
